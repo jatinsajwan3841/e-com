0e219a23e84ee4e89ceb0c28fb53846705ee5dfd
jatinsajwan3841 <jatinsajwan3841@gmail.com>
2021-11-16 17:49:49 +0530

    redux devtools error

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import rootReducer from "./reducers/index";
 let initialState = {};
 const composedEnhancer = compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const store = createStore(rootReducer, initialState, composedEnhancer);
